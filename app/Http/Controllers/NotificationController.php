df447dcf90a6ddf65db73fc0521de945a6d18530
Akhmad Muzanni Safi'i <akhmadmuzannisafii@gmail.com>
2020-10-01 14:47:07 +0700

    Create FCM Helper

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use FCM;
 
 class NotificationController extends Controller
 {
@@ -35,24 +36,16 @@ class NotificationController extends Controller
      */
     public function store(Request $request)
     {
-        $headers = [
-            'Content-type' => 'application/json',
-            'Authorization'=> 'key=AAAAq5PEITQ:APA91bE9Z7KmH5BDUi_fQJ8KCId7g0hdfrW8tEVmhRHwR4l7AtVwKFiNKJc3oklbkcSAFRvFqipPPKKwarYwICVcHCti0_QdeDbduDcHX6_3KpuqgeMc4C6l5-4Kw0UNolt1SViVXFCh',
+        $data = [
+            "title" => "Notif Penting A",
+            "message" => "Harusnya Tetap Bisa",
+            "user_id" => 19,
+            "tutor_id" => 30,
+            'token_recipient' => "eJLGS7DcQgab5nSaXaLI0P:APA91bGbcPEVfSdgbCU1iiqfq0YZDR_0iuyJ6lVeQwsBfojT2bj-_h6ksvPJOwIJ1W8HuSySANRltHUXZZ7YvNiDMEIlHrK8FuMYHjNHcX4zrztf3EfaABM0UESILgsaGbEr3sGxyiCZ"
         ];
-
-        $body = [
-            'data' => [
-                "title" => "Notif Penting A",
-                "message" => "Harusnya Tetap Bisa",
-                "user_id" => 19,
-                "tutor_id" => 30,
-            ],
-            'to' => "eJLGS7DcQgab5nSaXaLI0P:APA91bGbcPEVfSdgbCU1iiqfq0YZDR_0iuyJ6lVeQwsBfojT2bj-_h6ksvPJOwIJ1W8HuSySANRltHUXZZ7YvNiDMEIlHrK8FuMYHjNHcX4zrztf3EfaABM0UESILgsaGbEr3sGxyiCZ"
-        ];
-
-        $response = Http::withHeaders($headers)->post('https://fcm.googleapis.com/fcm/send', $body);
-
+        $response = FCM::pushNotification($data);
         return $response;
+
     }
 
     /**
--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\FirebaseNotifServiceProvider::class,
 
     ],
 
@@ -228,6 +229,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class, 
         'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+        'FCM' => App\Helpers\FirebaseNotification::class,
 
     ],
 
