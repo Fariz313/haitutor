7d1bb5597561ba01808f461f0dae2e01af428ebe
Andi Surya <andi502.as@gmail.com>
2020-10-14 09:44:21 +0700

    Change agora config to haitutor account

--- app/RoomVC.php
+++ app/RoomVC.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class RoomVC extends Model
 {
-    
+
+    const AGORA_APP_ID = "9dca2f48c8174aa7a0a172cc235a9960";
+    const AGORA_APP_CERFITICATE = "4e424f6ab3f242cb995a881deedb18fa";
+
     protected $table = "room_vc";
 
     public function user()
