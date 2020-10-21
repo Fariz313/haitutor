0e6ab9aac520446305bb82d603c1e5e32215291e
Andi Surya <andi502.as@gmail.com>
2020-10-21 14:19:59 +0700

    Change article and faq api (temporary)

--- routes/api.php
+++ routes/api.php
@@ -26,9 +26,9 @@ Route::middleware(['cors'])->group(function(){
     Route::put('balance', 'UserController@updateBalance');
     Route::put('firebase_token', 'UserController@updateFirebaseToken');
 
-    Route::get('faq','AskController@getAllFAQ');
+    Route::get('getFaq','AskController@getAllFAQ');
 
-    Route::get('article','ArticleController@getArticle');
+    Route::get('getArticle','ArticleController@getArticle');
     Route::post('article','ArticleController@store');
 
     Route::get('get_tutor', 'TutorController@getTutor');
