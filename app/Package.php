9e5a818ef9001b7d15792da2e86800757d41a3a3
Dessyafa Aulia Wardana <dessyafaauliawardana@gmail.com>
2020-11-11 13:03:53 +0700

    update package relation

--- app/Package.php
+++ app/Package.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Package extends Model
 {
     protected $table = 'package';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
