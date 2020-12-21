f215c97b9798c5202a54fd0485e450d203dce0c7
Akhmad Muzanni Safi'i <akhmadmuzannisafii@gmail.com>
2020-12-21 10:14:56 +0700

    Role Management Migration

---
+++ database/migrations/2020_12_21_094749_create_menu_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("action_url");
+            $table->string("action_method");
+            $table->integer("order");
+            $table->integer("is_menu");
+            $table->integer("is_deleted");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu');
+    }
+}
