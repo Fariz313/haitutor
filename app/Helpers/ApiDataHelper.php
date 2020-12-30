8fc9e5fe6408a89da52faf5ea51d7f88411c4012
Akhmad Muzanni Safi'i <akhmadmuzannisafii@gmail.com>
2020-12-30 12:23:32 +0700

    Access Menu in Production Database

---
+++ app/Helpers/ApiDataHelper.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Helpers;
+
+use App;
+use Illuminate\Support\Facades\Http;
+
+class ApiDataHelper {
+
+    const Environment = [
+        "DEVELOPMENT"   => 'development',
+        "PRODUCTION"    => 'production'
+    ];
+
+    public static function getApi($action_url, $action_method) {
+        $body = [
+            "action_url" => $action_url,
+            "action_method" => $action_method,
+        ];
+
+        $response = Http::get('http://haitutor.id/backend-educhat/api/list/allowed', $body);
+        return json_decode($response);
+    }
+
+    public static function getEnvironment()
+    {
+        if (App::environment("local")) {
+            return "development";
+        } else if (App::environment("production")) {
+            return "production";
+        }
+    }
+}
