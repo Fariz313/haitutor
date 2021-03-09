a4a9d60eeaaa6c1ea9d0c9a774b9527050f1cea3
Andi Surya <andi502.as@gmail.com>
2021-03-09 09:22:05 +0700

    FIx response helper

--- app/Helpers/ResponseHelper.php
+++ app/Helpers/ResponseHelper.php
@@ -7,13 +7,13 @@ class ResponseHelper
     public static function response($message, $data, $code, $status) {
         try {
             return response()->json([
-                "status" => $status ? "Error" : "error",
+                "status" => $status ?: "error",
                 "message" => $message,
                 "data" => $data,
             ], $code);
         } catch (\Throwable $th) {
             return response()->json([
-                "status" => $status ? "Error" : "error",
+                "status" => $status ?: "error",
                 "message" => $message,
             ], $code);
         }
